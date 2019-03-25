3aae3c2727be24314c2f83c6fe8a166b11faf3f0
Rukaiyaa Arssath <sarssath6520.student@hsfi.us>
2019-03-25 22:11:41 +0000

    ajax

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+  $.ajax({
+    url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+    method: "GET",
+        success: function(response) {
+            $("body").append("<img src='" + response.data[0].url + "'>");
+        }
+  });
 });
 
