5083d3f19d56cbe71827f5921f62efe4b6f21bab
Rukaiyaa Arssath <sarssath6520.student@hsfi.us>
2019-04-01 22:11:40 +0000

    Changes

--- js/script.js
+++ js/script.js
@@ -4,12 +4,26 @@
 /* global $ */
 
 $("#search-button").click(function(){
+  var thing = $("#search-term").val();
   $.ajax({
-    url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+    url: "https://api.giphy.com/v1/gifs/search?q=" + thing + "&rating=pg&api_key=dc6zaTOxFJmzC",
     method: "GET",
         success: function(response) {
-            $("body").append("<img src='" + response.data[0].url + "'>");
+          console.log(response.data);
+          var whatever = Math.floor(Math.random()*response.data.length);
+            $(".contain").html("<img src='" + response.data[whatever].images.original.url + "'>");
+          setClipboard(response.data[whatever].images.original.url);
         }
   });
 });
+
+function setClipboard(value) {
+    var tempInput = document.createElement("input");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+}
 
